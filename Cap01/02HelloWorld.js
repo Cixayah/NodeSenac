de7171da5257e895aff6bed902a5a947e82d396e
cix <gabrielviniciodacosta@gmail.com>
2024-04-13 10:49:34 -0300

    #

--- Cap01/02HelloWorld.js
+++ Cap01/02HelloWorld.js
@@ -3,5 +3,6 @@ const readline = require('readline');
 
 // readline é um módulo com a funcionalidade de receber entradas no console
 const rl = require.createInterface({
-    
+    input: process.stdin,
+    output: process.stdout
 });
