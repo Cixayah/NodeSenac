499999f3ccb9aac09319140f6d7499972c7397e9
cix <gabrielviniciodacosta@gmail.com>
2024-04-13 11:41:06 -0300

    #

--- Cap02/03HelloWorld.js
+++ Cap02/03HelloWorld.js
@@ -2,6 +2,6 @@ let prompt = require('prompt-sync')
 prompt = prompt({ sigint: true })
 console.log('Hello, World!\r\n')
 
-let name = prompt('Qual seu nome?')
+let name = prompt('Qual seu nome? ')
 console.log(`Olá ${name}! \r\n`)
 console.log('Hello, World!')
