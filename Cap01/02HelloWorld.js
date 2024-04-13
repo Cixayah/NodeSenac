4f3d7b2207e863b19f3a49b8bd45af5c8451f450
cix <gabrielviniciodacosta@gmail.com>
2024-04-13 10:47:48 -0300

    #

---
+++ Cap01/02HelloWorld.js
@@ -0,0 +1,7 @@
+// require importação de modulo
+const readline = require('readline');
+
+// readline é um módulo com a funcionalidade de receber entradas no console
+const rl = require.createInterface({
+    
+});
