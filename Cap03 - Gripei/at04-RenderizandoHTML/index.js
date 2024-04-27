570cd84be0dabcfa10477f0a7ecbe2c057b0c977
cix <gabrielviniciodacosta@gmail.com>
2024-04-27 09:52:57 -0300

    #

--- Cap03 - Gripei/at04-RenderizandoHTML/index.js
+++ Cap03 - Gripei/at04-RenderizandoHTML/index.js
@@ -6,6 +6,21 @@ const port = 3200
 const server = http.createServer((req, res) => {
     //Pega a substring após a "/"
     let pagina = q.pathname.substring(1)
+
     //Aponta url inicial para o index.html
     pagina = pagina == '' ? 'index.html' : pagina
+
+    //Insere a extensão HTML caso não for especificado
+    pagina = !pagina.includes('html') ? pagina = pagina + '.html' : pagina
+    console.log(pagina)
+
+    if (fs.existsSync(pagina)) { //Verifica se o arquivo existe
+        fs.readFile(pagina, function (err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.write(data)
+            return res.end()
+        })
+    } else {
+
+    }
 })
