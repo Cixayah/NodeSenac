8ef7be9c086f12b14548c0c2d65b199d2e4ba5d9
cix <gabrielviniciodacosta@gmail.com>
2024-04-27 08:08:35 -0300

    #

--- Cap03 - Gripei/at01-FsLerGravarJSON.js/fsJSON.js
+++ Cap03 - Gripei/at01-FsLerGravarJSON.js/fsJSON.js
@@ -1,21 +1,21 @@
 const fs = require('fs')
 module.exports = {
-     lerJSON: (arquivo)=>{
+    lerJSON: (arquivo) => {
         //Lê arquivos de texto de forma síncrona
         let json = fs.readFileSync(arquivo, 'utf8')
         return json
     },
-     converterJSON_Obj: (json)=>{
+    converterJSON_Obj: (json) => {
         //Converte string JSON para um objeto javaScript
         let dados = JSON.parse(json)
         return dados
     },
-     converterObj_JSON:(dados)=>{
+    converterObj_JSON: (dados) => {
         //Converte objeto javaScrip para uma string JSON
         let json = JSON.stringify(dados)
         return json
     },
-     salvarJSON:(json, arquivo)=>{
+    salvarJSON: (json, arquivo) => {
         //Grava arquivos de texto de forma síncrona
         fs.writeFileSync(arquivo, json)
     }
