5532ea41939a4deaa13e4d53d89f4758c0b7a566
cix <gabrielviniciodacosta@gmail.com>
2024-05-04 10:46:35 -0300

    #

--- Cap05/at02-AcessoAoBanco/index.js
+++ Cap05/at02-AcessoAoBanco/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const conn = require('./db')();
 const path = require('path')
-const baseDir = path.join(__dirname, 'template')
+const baseDir = path.join(__dirname, 'templates')
 var port = '3200'
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -11,4 +11,23 @@ app.get('/', (req, res) => res.sendFile(`${baseDir}/index.html`))
 app.get('/cadastrar', (req, res) => res.sendFile(`${baseDir}/cadastrar.html`))
 app.get('/registros', (req, res) => {
     const sql = 'SELECT * FROM pets;'
+
+    try {
+        conn.query(sql, (erro, dados) => {
+            if (erro) resposta = {
+                ...erro, status: 400, message: `Erro ao consultar os dados.`
+            }
+            else resposta = {
+                ...dados, status: 200, message: `Sucesso!`
+            }
+            res.json(resposta).status(200)
+        })
+    } catch (error) {
+        console.log(error)
+    }
 })
+
+app.listen(port, () => console.log
+    (
+        `Servidor rodando em: http://localhost:${port}`
+    ))
