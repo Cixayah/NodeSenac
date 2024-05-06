99463451cd1bed3407637da1badeb80091354403
Cix <gabrielviniciodacosta@gmail.com>
2024-05-06 10:04:18 -0300

    deixei bonito

--- Cap05/at02-AcessoAoBanco/db.js
+++ Cap05/at02-AcessoAoBanco/db.js
@@ -1,13 +1,27 @@
-const mysql = require('mysql2')
+// const mysql = require('mysql2')
+
+// module.exports = () => {
+//     const conn = mysql.createConnection({
+//         host: 'localhost',
+//         user: 'root',
+//         password: '',
+//         database: 'amigo_do_pet'
+//     });
+//     console.log('Funcionou')
+//     return conn;
+// }
+
+const mysql = require('mysql2');
 require('dotenv').config();
 
 module.exports = () => {
     const conn = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'amigo_do_pet'
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        port: process.env.DB_PORT
     });
-    console.log('Funcionou')
+    console.log('A conexão com o database Railway funcionou!');
     return conn;
-}
+};
