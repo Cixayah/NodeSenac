9604b7b741ec37b13ba619f6a8e24e4adce62254
cix <gabrielviniciodacosta@gmail.com>
2024-05-25 08:17:25 -0300

    #

--- Cap06/at03-APIRest_Amigo_do_Pet-V_1_1/index_antesRefator.js
+++ Cap06/at03-APIRest_Amigo_do_Pet-V_1_1/index_antesRefator.js
@@ -6,20 +6,20 @@ const doacao = new require('./models/doacao')
 
 var porta = '3200'
 
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.get('/', (req, res)=>res.send('API - Amigo do Pet'))
+app.get('/', (req, res) => res.send('API - Amigo do Pet'))
 
-app.get('/consultar/usuarios/:id?', async (req, res)=>{
+app.get('/consultar/usuarios/:id?', async (req, res) => {
     try {
-        let dados = req.params.id? await usuario.findOne({where:{id:req.params.id}}) : await usuario.findAll()
+        let dados = req.params.id ? await usuario.findOne({ where: { id: req.params.id } }) : await usuario.findAll()
         res.json(dados).status(200)
     } catch (error) {
         res.json(error).status(400)
     }
 })
-app.post('/cadastar/usuarios', async (req, res)=>{
+app.post('/cadastar/usuarios', async (req, res) => {
     try {
         let dados = req.body
         let respBd = await usuario.create(dados)
@@ -32,7 +32,7 @@ app.put('/atualizar/usuarios/:id', async (req, res) => {
     try {
         let id = req.params.id
         let dados = req.body
-        let respBd = await usuario.update(dados, {where:{id:id}})
+        let respBd = await usuario.update(dados, { where: { id: id } })
         res.json(respBd)
     } catch (error) {
         res.json(error).status(400)
@@ -41,11 +41,11 @@ app.put('/atualizar/usuarios/:id', async (req, res) => {
 app.delete('/excluir/usuarios/:id', async (req, res) => {
     try {
         let id = req.params.id
-        let respBd = await usuario.destroy({where:{id:id}})
+        let respBd = await usuario.destroy({ where: { id: id } })
         res.json(respBd)
     } catch (error) {
         res.json(error).status(400)
     }
 })
 
-app.listen(porta, ()=>console.log(`Servidor rodando em: http://localhost:${porta}`))
+app.listen(porta, () => console.log(`Servidor rodando em: http://localhost:${porta}`))
