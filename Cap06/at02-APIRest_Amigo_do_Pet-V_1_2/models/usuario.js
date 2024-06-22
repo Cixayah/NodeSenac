6d4b435d28b6b24aac991fd004fdfececfc17952
cix <gabrielviniciodacosta@gmail.com>
2024-06-22 10:51:39 -0300

    #

--- Cap06/at02-APIRest_Amigo_do_Pet-V_1_2/models/usuario.js
+++ Cap06/at02-APIRest_Amigo_do_Pet-V_1_2/models/usuario.js
@@ -1,38 +1,38 @@
 const { DataTypes, Model } = require('sequelize')
 const sequelize = require('../bdConexao')
-class Usuario extends Model{}
+class Usuario extends Model { }
 Usuario.init({
-    nome: {
-        type: DataTypes.STRING(50),
-        allowNull: false
-      },
-      cpf: {
-        type: DataTypes.STRING(14),
-        allowNull:false
-      },
-      telefone: {
-        type: DataTypes.STRING(14),
-        allowNull:true
-      },
-      whatsapp: {
-        type: DataTypes.STRING(14),
-        allowNull:true
-      },
-      email: {
-        type: DataTypes.STRING(50),
-        allowNull:false
-      },
-      senha: {
-        type: DataTypes.STRING,
-        allowNull:false
-      }
-},{
-    sequelize,
-    modelName:'usuario'
+  nome: {
+    type: DataTypes.STRING(50),
+    allowNull: false
+  },
+  cpf: {
+    type: DataTypes.STRING(14),
+    allowNull: false
+  },
+  telefone: {
+    type: DataTypes.STRING(14),
+    allowNull: true
+  },
+  whatsapp: {
+    type: DataTypes.STRING(14),
+    allowNull: true
+  },
+  email: {
+    type: DataTypes.STRING(50),
+    allowNull: false
+  },
+  senha: {
+    type: DataTypes.STRING,
+    allowNull: false
   }
+}, {
+  sequelize,
+  modelName: 'usuario'
+}
 )
 sequelize.sync()
- module.exports = Usuario   
+module.exports = Usuario
 
 /*
 
