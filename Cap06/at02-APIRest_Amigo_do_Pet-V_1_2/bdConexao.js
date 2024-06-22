36770cdd24a5501c34902a22ad1f83dd35e27740
cix <gabrielviniciodacosta@gmail.com>
2024-06-22 10:40:31 -0300

    #

--- Cap06/at02-APIRest_Amigo_do_Pet-V_1_2/bdConexao.js
+++ Cap06/at02-APIRest_Amigo_do_Pet-V_1_2/bdConexao.js
@@ -1,17 +1,18 @@
-const {Sequelize} = require('sequelize');
+const { Sequelize } = require('sequelize');
+
 const sequelize = new Sequelize(
-    'amigo_do_pet2', 'nodejs', '1234', {
-        host:'localhost',
-        dialect:'mysql',
-        charset: 'utf8',
-        collate: 'utf8_general_ci',
-        timezone:"-03:00"
-    })
+    'amigo_do_pet', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql',
+    charset: 'utf8',
+    collate: 'utf8_general_ci',
+    timezone: "-03:00"
+})
 try {
     sequelize.authenticate()
     console.log('Conectado ao banco')
 } catch (erro) {
-    console.log('Não foi possível conectar: ', erro )
+    console.log('Não foi possível conectar: ', erro)
 }
 module.exports = sequelize
 
